2d74dd89c34360116a35f0868fe42515ee903c4c
hu.junhua <hu.junhua@xiaoyi.com>
2017-04-01 10:51:49 +0800

    4. Median of Two Sorted Arrays speed up runtime complexity to O(log(min(m,n)))
    reduse once binary search when total is even

--- hard/Median of Two Sorted Arrays.h
+++ hard/Median of Two Sorted Arrays.h
@@ -45,17 +45,18 @@ public:
 			{
 				EvenResult even_result = binarySearchMedianEven(nums1,nums2);
 				int position = even_result.position;
+				int position2 = even_result.position2;
 				if (position == -1)
 				{
 					even_result = binarySearchMedianEven(nums2,nums1);
 					int position = even_result.position;
-					
-					int second_number = even_result.is_left ? getNext(nums2, nums1, position) : getLast(nums2, nums1, position);
+					int position2 = even_result.position2;
+					int second_number = even_result.is_left ? getNext(nums2, nums1, position, position2) : getLast(nums2, nums1, position, position2);
 					result_media = (double)(nums2[position]+second_number)/2;
 				}
 				else
 				{
-					int second_number = even_result.is_left ? getNext(nums1,nums2 ,position) : getLast( nums1, nums2, position);
+					int second_number = even_result.is_left ? getNext(nums1,nums2 ,position, position2) : getLast( nums1, nums2, position, position2);
 					result_media = (double)(nums1[position]+second_number)/2;
 				}
 			}
@@ -75,11 +76,11 @@ public:
 		}
 		return result_media;
     }
-	int getNext(vector<int>& nums1, vector<int>& nums2, int position)
+	int getNext(vector<int>& nums1, vector<int>& nums2, int position, int position2)
 	{
 		int size1 = nums1.size();
 		int size2 = nums2.size();
-		int nums2_p = binarySearchPosition(nums2, 0, size2 -1, nums1[position]);
+		int nums2_p = position2;//binarySearchPosition(nums2, 0, size2 -1, nums1[position]);
 		int result = nums1[position];
 			if (position < (size1 - 1) && nums2_p < size2 - 1)
 			{
@@ -95,9 +96,9 @@ public:
 			}
 			return result;
 	}
-	int getLast(vector<int>& nums1, vector<int>& nums2, int position)
+	int getLast(vector<int>& nums1, vector<int>& nums2, int position, int position2)
 	{
-		int nums2_p = binarySearchPosition(nums2, 0, nums2.size() - 1, nums1[position]);
+		int nums2_p = position2;//binarySearchPosition(nums2, 0, size2 -1, nums1[position]);
 		int result = nums1[position];
 
 		if (position > 0 && nums2_p+1 >0)
@@ -130,8 +131,10 @@ public:
 		int max_position2 = size2 -1;
 		int start1 = 0;
 		int end1 = max_position1;
+		int start2 = 0;
+		int end2 = max_position2;
 		int m1_p = (start1+end1)/2;
-		int m2_p = binarySearchPosition(nums2, 0, max_position2, nums1[m1_p]);
+		int m2_p = binarySearchPosition(nums2, start2, end2, nums1[m1_p]);
 		int total_smaller = m1_p+m2_p+1;
 		int total = size1+size2;
 		int smaller_number = total/2;
@@ -140,23 +143,25 @@ public:
 					if (total_smaller < smaller_number)
 					{
 						start1=m1_p+1;
+						start2 = m2_p;
 					}
 					else if(total_smaller > smaller_number)
 					{
 						end1 = m1_p-1;	
+						end2 = m2_p;
 					}
-					if(start1 > end1)
+					if(start1 > end1 || start2 > end2)
 					{
 						m1_p = -1;
 						break;
 					}
 					m1_p = (start1+end1)/2;
-					m2_p = binarySearchPosition(nums2, 0, max_position2, nums1[m1_p]);
+					m2_p = binarySearchPosition(nums2, start2, end2, nums1[m1_p]);
 					total_smaller = m1_p+m2_p+1;
 				}
 		return m1_p;
 	}
-	struct EvenResult { int position; bool is_left; };
+	struct EvenResult { int position; bool is_left; int position2; };
 	EvenResult binarySearchMedianEven(vector<int>& nums1, vector<int>& nums2)
 	{
 		int size1 = nums1.size();
@@ -165,14 +170,16 @@ public:
 		int max_position2 = size2 -1;
 		int start1 = 0;
 		int end1 = max_position1;
+		int start2 = 0;
+		int end2 = max_position2;
 		int m1_p = (start1+end1)/2;
-		int m2_p = binarySearchPosition(nums2, 0, max_position2, nums1[m1_p]);
+		int m2_p = binarySearchPosition(nums2, start2, end2, nums1[m1_p]);
 		int total_smaller = m1_p+m2_p+1;
 		int total = size1+size2;
 		int smaller_number = total/2;
 				while (total_smaller != smaller_number-1 && total_smaller != smaller_number)
 				{
-					if(start1 > end1)
+					if(start1 > end1 || start2 > end2)
 					{
 						m1_p = -1;
 						break;
@@ -181,19 +188,21 @@ public:
 					if (total_smaller < smaller_number-1)
 					{
 						start1=m1_p+1;
+						start2 = m2_p;
 					}
 					else if(total_smaller > smaller_number)
 					{
 						end1 = m1_p-1;	
+						end2 = m2_p;
 					}
 					m1_p = (start1+end1)/2;
-					m2_p = binarySearchPosition(nums2, 0, max_position2, nums1[m1_p]);
+					m2_p = binarySearchPosition(nums2, start2, end2, nums1[m1_p]);
 					total_smaller = m1_p+m2_p+1;
 				}
 				EvenResult result;
 				result.position = m1_p;
 				result.is_left = total_smaller == smaller_number-1;
-
+				result.position2 = m2_p;
 		return result;
 	}
 	//01234*5
